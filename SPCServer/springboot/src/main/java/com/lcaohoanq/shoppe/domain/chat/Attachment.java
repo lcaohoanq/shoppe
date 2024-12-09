d0c6ce456ac3413c3242a04b95e1bb8f1ffb600c
lcaohoanq <hoangclw@gmail.com>
2024-12-09 23:42:53 +0700

    feat: add Chat feature

---
+++ SPCServer/springboot/src/main/java/com/lcaohoanq/shoppe/domain/chat/Attachment.java
@@ -0,0 +1,39 @@
+package com.lcaohoanq.shoppe.domain.chat;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.lcaohoanq.shoppe.base.entity.BaseEntity;
+import com.lcaohoanq.shoppe.metadata.MediaMeta;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "attachments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Attachment {
+
+    @Id
+    @SequenceGenerator(name = "attachments_seq", sequenceName = "attachments_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "attachments_seq")
+    @Column(name="id", unique=true, nullable=false)
+    @JsonProperty("id")
+    private Long id;
+    
+    @Embedded
+    private MediaMeta mediaMeta;
+    
+}
