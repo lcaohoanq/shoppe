45ac5d08654c210a6841062aed13a9c9859dfcd0
lcaohoanq <hoangclw@gmail.com>
2024-12-12 09:49:56 +0700

    test: add unit test

---
+++ SPCServer/springboot/src/test/java/com/lcaohoanq/shoppe/util/DTOConverterTest.java
@@ -0,0 +1,61 @@
+package com.lcaohoanq.shoppe.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class DTOConverterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void toUserResponse() {
+    }
+
+    @Test
+    void toCategoryResponse() {
+    }
+
+    @Test
+    void toRoleResponse() {
+    }
+
+    @Test
+    void toOrderResponse() {
+    }
+
+    @Test
+    void toProductResponse() {
+    }
+
+    @Test
+    void toProductImageResponse() {
+    }
+
+    @Test
+    void toOrderDetailResponse() {
+    }
+
+    @Test
+    void toCartResponse() {
+    }
+
+    @Test
+    void toCartItemResponse() {
+    }
+
+    @Test
+    void toWalletResponse() {
+    }
+
+    @Test
+    void toWareHouseResponse() {
+    }
+}
