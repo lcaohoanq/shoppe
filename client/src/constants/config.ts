861ecebeca2d82462406c8dbf94e8435ca270a1e
lcaohoanq <hoangclw@gmail.com>
2025-03-09 11:48:08 +0700

    chore: add sample mock api source

--- client/src/constants/config.ts
+++ client/src/constants/config.ts
@@ -1,6 +1,7 @@
 const config = {
-  baseUrl: 'http://localhost:8080/api/v1/',
-  maxSizeUploadAvatar: 1048576 // bytes
-}
+  baseUrl: 'https://api-ecom.duthanhduoc.com/',
+  springBootUrl: 'http://localhost:8080/api/v1',
+  maxSizeUploadAvatar: 1048576, // bytes
+};
 
-export default config
+export default config;
