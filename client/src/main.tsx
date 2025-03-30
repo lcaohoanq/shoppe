bb26409272576fc9ce5f3e8d7cb01e8349d4697e
lcaohoanq <hoangclw@gmail.com>
2025-03-30 12:00:11 +0700

    fix: fix lost fe code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from 'src/App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import 'src/i18n/i18n'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AppProvider } from './contexts/app.context'
+import { createRoot } from 'react-dom/client'
+import { worker } from './msw/browser'
 import { I18nextProvider } from 'react-i18next'
-import i18n from './i18n/i18n'
+import i18n from 'src/i18n/i18n'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,8 +20,16 @@ const queryClient = new QueryClient({
   }
 })
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+async function enableMocking() {
+  if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'production') {
+    return worker.start({
+      onUnhandledRequest: 'bypass' // Don't warn about unhandled requests
+    })
+  }
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root') as HTMLElement).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <I18nextProvider i18n={i18n}>
@@ -28,5 +39,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         </I18nextProvider>
       </QueryClientProvider>
     </BrowserRouter>
-  </React.StrictMode>
-)
+  )
+})
