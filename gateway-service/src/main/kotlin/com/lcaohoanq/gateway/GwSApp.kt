b5912b24b88043e744c14553e8dd11880da3e90a
lcaohoanq <hoangclw@gmail.com>
2025-05-16 08:41:31 +0700

    chore: using own lib

--- gateway-service/src/main/kotlin/com/lcaohoanq/gateway/GwSApp.kt
+++ gateway-service/src/main/kotlin/com/lcaohoanq/gateway/GwSApp.kt
@@ -1,11 +1,9 @@
 package com.lcaohoanq.gateway
 
+import io.github.hoangclw.kotlinbrowserlauncher.openHomePage
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import java.awt.Desktop
-import java.net.URI
-import java.util.*
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -18,37 +16,5 @@ fun main(args: Array<String>) {
 
     if (!activeProfiles.contains("docker")) {
         openHomePage("http://localhost:4003/swagger-ui.html")
-    }
-}
-
-fun openHomePage(urls: Any) {
-    try {
-        val urlList = when (urls) {
-            is List<*> -> urls.filterIsInstance<String>() // Handles if it's a List<String>
-            is String -> listOf(urls) // Converts a single URL to a list
-            else -> throw IllegalArgumentException("Invalid argument type")
-        }
-
-        val desktop = if (Desktop.isDesktopSupported()) Desktop.getDesktop() else null
-        val os = System.getProperty("os.name").lowercase(Locale.getDefault())
-
-        for (url in urlList) {
-            if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
-                desktop.browse(URI.create(url))
-            } else {
-                when {
-                    os.contains("win") -> Runtime.getRuntime()
-                        .exec("rundll32 url.dll,FileProtocolHandler $url")
-
-                    os.contains("mac") -> Runtime.getRuntime().exec("open $url")
-                    os.contains("nix") || os.contains("nux") -> Runtime.getRuntime()
-                        .exec("xdg-open $url")
-
-                    else -> println("Unsupported operating system: $os")
-                }
-            }
-        }
-    } catch (e: Exception) {
-        e.printStackTrace()
     }
 }
