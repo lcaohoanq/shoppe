32c314b5c653731765b0a29ba656663627816486
lcaohoanq <hoangclw@gmail.com>
2025-05-24 22:52:51 +0700

    fix: fix vitest setup

--- react-client/vite.config.ts
+++ react-client/vite.config.ts
@@ -7,11 +7,6 @@ import { visualizer } from 'rollup-plugin-visualizer'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), visualizer()] as any,
-  test: {
-    environment: 'jsdom',
-    setupFiles: path.resolve(__dirname, './vitest.setup.ts'),
-    globals: true
-  },
   server: {
     host: true,
     port: 4000,
